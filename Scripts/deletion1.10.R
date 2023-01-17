3d4a756a865d71097a78741e694c787e6e0c74ad
Yang Ba <yang@Yangs-MacBook-Pro.local>
2023-01-16 22:09:56 -0700

    1.16update

--- Scripts/deletion1.10.R
+++ Scripts/deletion1.10.R
@@ -11,51 +11,12 @@ require(Matrix)
 library(dplyr)
 #install.packages('dplyr')
 
-options(scipen=999)
-
-simulated_df = simul
-colnames(simulated_df)
-
-#read data -prolific
-Prolific <- read_excel("Dropbox (ASU)/Code/8.3/Prolific.xlsx")
-#colnames(simulated_df)[2] <- 'workerid'
-colnames(simulated_df)[2] <- 'imageid'
-#colnames(simulated_df)[1] <- 'workerid'
-#colnames(simulated_df)[2] <- 'imageid'
-colnames(simulated_df)[1] <- 'workerid'
-colnames(Prolific)[2] <- 'workerid'
-colnames(Prolific)[8] <- 'imageid'
-colnames(Prolific)[18] <- 'decision'
-#simulated_df$decision <- ifelse(simulated_df$decision=='different', 1, 0)
-
-colnames(norm_df)[1] <- 'imageid'
-norm_df$decision <- ifelse(norm_df$decision=='different', 1, 0)
-
-colnames(spam_df)[1] <- 'imageid'
-spam_df$decision <- ifelse(spam_df$decision=='different', 1, 0)
-
-Prolific$decision <- ifelse(Prolific$decision=='different', 1, 0)
-
 
 # REM
-p = glmer(cbind(y, num_task - y) ~ (1 | worker),     
-          data = simulated_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
-summary(p)
-
-p1 = glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),     
-           data = norm_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
-summary(p1)
-
-p= glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),     
-         data = simulated_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
-summary(p)
-
-p = glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),    
-          data = Prolific, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
-summary(p)
-
 p = glmer(as.factor(decision) ~ (1 | workerid) + (1 | imageid) + (1|task:workerid),   
           data = dt, family = binomial)
+summary(p)
+
 trace <- function(A) {
   n <- dim(A)[1] # get dimension of matrix
   tr <- 0 # initialize trace value
@@ -70,7 +31,6 @@ trace <- function(A) {
 
 # trace
 #p.t = trace(v.matrix)
-
 
 
 # variance -covariance matrix 
@@ -92,7 +52,6 @@ p.t = sum(data.frame(VarCorr(p))$vcov)
 
 p.var.image = data.frame(VarCorr(p))$vcov[3] / p.t
 #p.var.imageint = (data.frame(VarCorr(p))$vcov[1] + data.frame(VarCorr(p))$vcov[4]) /p.t
-
 
 
 #p_ = exclude.influence(p, "imageid", '2395_1.jpg')
@@ -122,10 +81,10 @@ p_value <- c()
 
 
 
-p_t <- exclude.influence(p, "imageid", 1)
-n004201_0001_02.png
+#p_t <- exclude.influence(p, "imageid", 1)
+#n004201_0001_02.png
 
--2*(logLik(p)[1]- logLik(p_t)[1])
+#-2*(logLik(p)[1]- logLik(p_t)[1])
 
 for (x in unique(dt$workerid)) {
   workerid <- append(workerid, x)
@@ -164,9 +123,6 @@ length(disagreerate)
 length(p_value) 
 
 
-
-
-
 #df.p$trace = with(df.p, total.var - p.t)
 #df.p$varimageratio = with(df.p, var.image/total.var - p.var.image )
 #df.p$varimageintratio = with(df.p, varint.image/total.var - p.var.imageint)
@@ -198,16 +154,177 @@ df.p = data.frame(workerid,loglik, total.var, var.image, vartrace, vartrace2, va
 #names(Data.p) <- c('imageid', 'loglik', 'var.image', 'varint.image', 'total.var','var.image.change', 'varint.image.change', 'total.var.change', 'vartrace', 'vartrace2', 'vartrace3', 'disagreerate')
 
 
+# concatenate
+dfl = df.p[, c('workerid', 'loglik', 'vartrace', 'total.var.change')]%>% left_join(dtWorker, by = "workerid")
+dfl
+
+cor(dfl$loglik, abs(dfl$worker_eff)) # -0.6561019.      # -0.2752111
+cor(dfl$vartrace, abs(dfl$worker_eff))  # 0.6623779.    # 0.3852399
+cor(dfl$total.var.change, abs(dfl$worker_eff))  #-0.6623779.   # -0.3852399
+
+
+
+
+# image 
+imageid <- c()
+loglik <- c()
+total.var <- c()
+var.image <- c()
+varint.image <- c()
+vartrace <- c()
+vartrace2 <- c()
+vartrace3 <- c()
+total.var.change <- c()
+var.image.change <- c()
+varint.image.change <- c()
+disagreerate <- c()
+
+
+for (x in unique(dt$imageid)) {
+  imageid <- append(imageid, x)
+  p_temp <- exclude.influence(p, "imageid", x)
+  loglik <- append(loglik, -2*(logLik(p)[1]- logLik(p_temp)[1]))
+  
+  var.image  <- append(var.image, data.frame(VarCorr(p_temp))$vcov[3])
+  #varint.image <- append(varint.image , data.frame(VarCorr(p_temp))$vcov[1] + data.frame(VarCorr(p_temp))$vcov[5] + data.frame(VarCorr(p_temp))$vcov[4])
+  total.var <- append(total.var, sum(data.frame(VarCorr(p_temp))$vcov))
+  
+  var.image.change <- append(var.image.change, data.frame(VarCorr(p_temp))$vcov[3]/ sum(data.frame(VarCorr(p_temp))$vcov) - p.var.image )
+  #varint.image.change <- append(varint.image.change, (data.frame(VarCorr(p_temp))$vcov[1] + data.frame(VarCorr(p_temp))$vcov[3] + data.frame(VarCorr(p_temp))$vcov[4]) /sum(data.frame(VarCorr(p_temp))$vcov) - p.var.imageint)
+  
+  total.var.change <- append(total.var.change, sum(data.frame(VarCorr(p_temp))$vcov) / p.t)
+  
+  vartrace <- append(vartrace,  trace(v.matrix - as.matrix(Matrix::bdiag(VarCorr(p_temp)))))
+  vartrace2 <- append(vartrace2, trace(v.matrix - as.matrix(Matrix::bdiag(VarCorr(p_temp))))- rankMatrix(v.matrix)[1])
+  vartrace3 <- append(vartrace3, abs(trace(v.matrix - as.matrix(Matrix::bdiag(VarCorr(p_temp))))- rankMatrix(v.matrix)[1]))
+  disagreerate <- append(disagreerate, min(table(dt[dt$imageid==x, ]$decision))/sum(table(dt[dt$imageid==x, ]$decision)))
+}
+
+
+length(imageid)
+length(loglik)
+length(total.var)
+length(var.image)
+length(varint.image)
+length(vartrace)
+length(vartrace2)
+length(vartrace3)
+length(total.var.change)
+length(var.image.change)
+length(varint.image.change)
+length(disagreerate) 
+
+
+
+df.i = data.frame(imageid,loglik, total.var, var.image, vartrace, vartrace2, vartrace3,
+                  total.var.change, var.image.change, disagreerate)
+
+df.i$disagreerate <- ifelse(df.i$disagreerate ==1, 0, df.i$disagreerate)
+
+
+cor(df.i$loglik, df.i$disagreerate)  # 0.4203562.      # 0.8705909
+cor(df.i$vartrace, df.i$disagreerate) # -0.4889774.   # -0.7145904
+cor(df.i$total.var.change, df.i$disagreerate) # 0.4889774.  # 0.7145904
+
+dfi = df.i[, c('imageid', 'loglik', 'vartrace', 'total.var.change')]%>% left_join(dtImage, by = "imageid")
+dfi
+
+cor(dfi$loglik, abs(dfi$image_eff)) # -0.3665448.   # -0.8620711
+cor(dfi$vartrace, abs(dfi$image_eff))  # 0.4603978.  # 0.7760047
+cor(dfi$total.var.change, abs(dfi$image_eff))  #-0.4603978. # -0.7760047
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# past codes
+
+options(scipen=999)
+
+simulated_df = simul
+colnames(simulated_df)
+
+
+
+
+#read data -prolific
+Prolific <- read_excel("Dropbox (ASU)/Code/8.3/Prolific.xlsx")
+#colnames(simulated_df)[2] <- 'workerid'
+colnames(simulated_df)[2] <- 'imageid'
+#colnames(simulated_df)[1] <- 'workerid'
+#colnames(simulated_df)[2] <- 'imageid'
+colnames(simulated_df)[1] <- 'workerid'
+colnames(Prolific)[2] <- 'workerid'
+colnames(Prolific)[8] <- 'imageid'
+colnames(Prolific)[18] <- 'decision'
+#simulated_df$decision <- ifelse(simulated_df$decision=='different', 1, 0)
+
+colnames(norm_df)[1] <- 'imageid'
+norm_df$decision <- ifelse(norm_df$decision=='different', 1, 0)
+
+colnames(spam_df)[1] <- 'imageid'
+spam_df$decision <- ifelse(spam_df$decision=='different', 1, 0)
+
+Prolific$decision <- ifelse(Prolific$decision=='different', 1, 0)
+
+
+
+
+
+
+
+p = glmer(cbind(y, num_task - y) ~ (1 | worker),     
+          data = simulated_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
+summary(p)
+
+p1 = glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),     
+           data = norm_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
+summary(p1)
+
+p= glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),     
+         data = simulated_df, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
+summary(p)
+
+p = glmer(decision ~ (1 | workerid) + (1 | task/imageid) + (1 | imageid:workerid) + (1 | task:workerid),    
+          data = Prolific, family = binomial, control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e6)), nAGQ = 1)
+summary(p)
+
+
+
+
 df.p$inf = ifelse(df.p$p_value>0.05, 1, 0)
 
-df.p[c(workerid, loglik), ]
+df.p[, c('workerid', 'loglik')]
 df.p[order(df.p$loglik, decreasing = TRUE), ]
 
+df.p[order(df.p$loglik, decreasing = TRUE), ]
 
+df.p
 
 sort(df.p$loglik)
 
 plot(df.p$loglik, df.p$p_value)
+
+
+# concatenate
+dfl = df.p[, c('workerid', 'loglik')]%>% left_join(dtWorker, by = "workerid")
+dfl
+
+cor(dfl$loglik, abs(dfl$worker_eff))
+
+
+
+
+
 
 ggplot(data = df.p, aes(x = workerid, y = loglik)) +
   stat_poly_line() +
